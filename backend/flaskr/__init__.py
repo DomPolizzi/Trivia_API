65456fc75f06a41763c1e80bb5855792e46eb9b0
Finnvoid <neku113@gmail.com>
2020-03-11 00:09:16 -0400

    inital config

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -11,15 +11,16 @@ QUESTIONS_PER_PAGE = 10
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
+  cors = CORS(app, resources={r"/api/*": {"origins" : "*" }})
   setup_db(app)
   
-  '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
-
-  '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
+  
+  @app.after request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+    return response  
+  
 
   '''
   @TODO: 
